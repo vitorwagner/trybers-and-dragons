9eb4a840deacbc45b4cd5feacaa4bd2e85236124
Vitor Wagner <vitormwagner@gmail.com>
2023-05-06 17:15:28 -0300

    feat: :sparkles: Added PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,21 @@
 import Battle from './Battle';
+import Fighter from '../Fighter';
 
 export default class PVP extends Battle {
-  
+  private _player: Fighter;
+  private _opponent: Fighter;
+
+  constructor(player: Fighter, opponent: Fighter) {
+    super(player);
+    this._player = player;
+    this._opponent = opponent;
+  }
+
+  public fight(): number {
+    while (this._player.lifePoints > 0 && this._opponent.lifePoints > 0) {
+      this._player.attack(this._opponent);
+      this._opponent.attack(this._player);
+    }
+    return super.fight();
+  }
 }
